88850c1564c7ea4631038f0301ac1415fe1e5de5
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-01-26 17:01:20 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video 1 
+//            using System;
+
+//// Class Example
+//class MyClass
+//        {
+//            public int X { get; set; }
+//            public int Y { get; set; }
+//        }
+
+//        // Struct Example
+//        struct MyStruct
+//        {
+//            public int X { get; set; }
+//            public int Y { get; set; }
+//        }
+
+//        class Program
+//        {
+//            static void Main()
+//            {
+//                // Class is a reference type
+//                MyClass objClass = new MyClass { X = 10, Y = 20 };
+//                MyClass objClass2 = objClass;
+//                objClass2.X = 30;
+//                Console.WriteLine($"Class: objClass.X = {objClass.X}, objClass2.X = {objClass2.X}");
+
+//                // Struct is a value type
+//                MyStruct objStruct = new MyStruct { X = 10, Y = 20 };
+//                MyStruct objStruct2 = objStruct;
+//                objStruct2.X = 30;
+//                Console.WriteLine($"Struct: objStruct.X = {objStruct.X}, objStruct2.X = {objStruct2.X}");
+//            }
+//        }
+        #endregion
+
+        #region Video 2 
+
+        #endregion
+
+        #region Video 3
+
+        #endregion
+
+
+        #region Video 4
+
+        #endregion
+
+
+        #region Video 5
+
+        #endregion
+
+        #region Video 6
+
+        #endregion
+
+        #region Video 7
+
+        #endregion
+
+        #region Video 8
+
+        #endregion
+
+        #region Video 9
+
+        #endregion
+    }
+    }
+}
